d96a985bd8af59b8eb9adcc5238fc5844f34f2c1
luisa-charles <39881835+luisa-charles@users.noreply.github.com>
2019-11-22 14:36:18 +0000

    Added product images. Added carosel on about us page (it doesn't work properly yet)
    Added scripts to top of about us page to facilitate bootstrap and jquery. Added product images for hair to images folder. Added carosel on about us page next to main paragraph (still needs captions adding and size fixing). Cleaned up code

--- js/API.js
+++ js/API.js
@@ -1,48 +1,45 @@
-$(document).ready(function () {
+$(document).ready(function() {
+  // Get Location
+  navigator.geolocation.getCurrentPosition(success, error);
 
-    // Get Location
-    navigator.geolocation.getCurrentPosition(success, error);
+  function success(pos) {
+    var lat = pos.coords.latitude;
+    var long = pos.coords.longitude;
+    weather(lat, long);
+  }
 
-    function success(pos) {
-        var lat = pos.coords.latitude;
-        var long = pos.coords.longitude;
-        weather(lat, long);
-    }
+  function error() {
+    console.log("There was an error");
+  }
 
-    function error() {
-        console.log('There was an error');
-    }
+  // Call Weather
+  function weather(lat, long) {
+    var fixedLat = lat.toFixed(2);
+    var fixedLong = long.toFixed(2);
+    console.log(fixedLat, fixedLong);
+    var URL = `https://fcc-weather-api.glitch.me/api/current?lat=${fixedLat}&lon=${fixedLong}`;
 
-    // Call Weather
-    function weather(lat, long) {
-      const fixedLat = lat.toFixed(2);
-      const fixedLong = long.toFixed(2);
-      console.log(fixedLat, fixedLong)
-        var URL = `https://fcc-weather-api.glitch.me/api/current?lat=${fixedLat}&lon=${fixedLong}`;
+    $.getJSON(URL, function(data) {
+      updateDOM(data);
+    });
+  }
 
-        $.getJSON(URL, function(data) {
-            updateDOM(data);
-        });
-    }
+  // Update Dom
+  function updateDOM(data) {
+    var city = data.name;
+    var temp = Math.round(data.main.temp_max);
+    var desc = data.weather[0].description;
+    var icon = data.weather[0].icon;
 
-    // Update Dom
-    function updateDOM(data) {
-        var city = data.name;
-        var temp = Math.round(data.main.temp_max);
-        var desc = data.weather[0].description;
-        var icon = data.weather[0].icon;
+    $("#city").html(city);
+    $("#temp").html(temp);
+    $("#desc").html(desc);
+    $("#icon").attr("src", icon);
+  }
 
-        $('#city').html(city);
-        $('#temp').html(temp);
-        $('#desc').html(desc);
-        $('#icon').attr('src', icon);
-    }
+  var dt = new Date();
+  document.getElementById("time").innerHTML = dt.toLocaleTimeString();
 
-    var dt = new Date();
-    document.getElementById("time").innerHTML = dt.toLocaleTimeString();
-
-    var dt = new Date();
-    document.getElementById("date").innerHTML = dt.toLocaleDateString();
-
-
+  var dt = new Date();
+  document.getElementById("date").innerHTML = dt.toLocaleDateString();
 });
